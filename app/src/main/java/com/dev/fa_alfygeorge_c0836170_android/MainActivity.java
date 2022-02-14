aa8435e613b58faca90a5b677268564b0bd6654b
Alfy524 <alfy524@gmail.com>
2022-02-14 17:59:44 -0500

    1st commit

---
+++ app/src/main/java/com/dev/fa_alfygeorge_c0836170_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dev.fa_alfygeorge_c0836170_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
